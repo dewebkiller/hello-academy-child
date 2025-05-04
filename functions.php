28917fee7bbe4a06a1551ad6f87c43248ac3d88f
dewebkiller <dewebkiller@gmail.com>
2025-05-04 23:48:42 +0545

    style change forthe subscription

--- functions.php
+++ functions.php
@@ -1,22 +1,29 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'hello_academy_child_enqueue_styles' );
-	 function hello_academy_child_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
+<?php
+add_action( 'wp_enqueue_scripts', 'hello_academy_child_enqueue_styles' );
+function hello_academy_child_enqueue_styles() {
+    // Enqueue parent style
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
+    // Enqueue child style, after parent
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style'), // Make sure it loads after the parent
+        wp_get_theme()->get('Version') // Use child theme version for cache busting
+    );
+}
 
-			 add_filter( 'get_the_archive_title', function ($title) {    
-        if ( is_category() ) {    
-                $title = single_cat_title( '', false );    
-            } elseif ( is_tag() ) {    
-                $title = single_tag_title( '', false );    
-            } elseif ( is_author() ) {    
-                $title = '<span class="vcard">' . get_the_author() . '</span>' ;    
-            } elseif ( is_tax() ) { //for custom post types
-                $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
-            } elseif (is_post_type_archive()) {
-                $title = post_type_archive_title( '', false );
-            }
-        return $title;    
-    });
- ?>
+// Optional: Customize archive titles
+add_filter( 'get_the_archive_title', function ($title) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif ( is_tax() ) {
+        $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    }
+    return $title;
+});
