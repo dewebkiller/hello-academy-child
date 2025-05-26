6b17eb2888e4e80fa6667d6fe70b6f7499e81a18
dewebkiller <dewebkiller@gmail.com>
2025-05-26 23:42:06 +0545

    add dropdown menu for the dashboard via custom code

--- functions.php
+++ functions.php
@@ -1,13 +1,11 @@
 <?php
-add_action('wp_enqueue_scripts', 'hello_academy_child_enqueue_styles');
-function hello_academy_child_enqueue_styles()
-{
+add_action( 'wp_enqueue_scripts', 'hello_academy_child_enqueue_styles' );
+function hello_academy_child_enqueue_styles() {
     // Enqueue parent style
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
     // Enqueue child style, after parent
-    wp_enqueue_style(
-        'child-style',
+    wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array('parent-style'), // Make sure it loads after the parent
         wp_get_theme()->get('Version') // Use child theme version for cache busting
@@ -15,17 +13,17 @@ function hello_academy_child_enqueue_styles()
 }
 
 // Optional: Customize archive titles
-add_filter('get_the_archive_title', function ($title) {
-    if (is_category()) {
-        $title = single_cat_title('', false);
-    } elseif (is_tag()) {
-        $title = single_tag_title('', false);
-    } elseif (is_author()) {
+add_filter( 'get_the_archive_title', function ($title) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
         $title = '<span class="vcard">' . get_the_author() . '</span>';
-    } elseif (is_tax()) {
-        $title = sprintf(__('%1$s'), single_term_title('', false));
-    } elseif (is_post_type_archive()) {
-        $title = post_type_archive_title('', false);
+    } elseif ( is_tax() ) {
+        $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
     }
     return $title;
 });
@@ -53,6 +51,30 @@ function add_dynamic_user_avatar_style() {
 }
 add_action('wp_head', 'add_dynamic_user_avatar_style');
 
+function move_submenu_inside_container() {
+    ?>
+    <script>
+        jQuery(document).ready(function($) {
+            function repositionSubmenu() {
+                var $container = $('.dropdown-wrapper'); // your main container
+                var $submenu = $('.hfe-has-submenu-container .sub-menu').first();
+
+                if ($container.length && $submenu.length) {
+                    $submenu.appendTo($container);
+                }
+            }
+
+            // Run once on load
+            repositionSubmenu();
+
+            // Optional: run again after a short delay in case menu loads asynchronously
+            setTimeout(repositionSubmenu, 1000);
+        });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'move_submenu_inside_container');
+
 /* Use shortcode [dwk_getloggedinusers] */
 add_shortcode('dwk_getloggedinusers', 'dwk_get_loggedin_users');
 function dwk_get_loggedin_users()
@@ -61,7 +83,7 @@ function dwk_get_loggedin_users()
     global $current_user;
     wp_get_current_user();
     if (is_user_logged_in()) {
-        echo '<strong>' . $current_user->display_name . '</strong>';
+     echo '<strong>' . $current_user->display_name . '</strong>';
     }
     $output = ob_get_contents();
     ob_get_clean();
--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,5 +1,12 @@
 
     jQuery(document).ready(function($) {
+            $('.dropdown-wrapper').on('click', function(e) {
+                e.preventDefault();
+                var $menu = $(this).find('.sub-menu').first();
+                $menu.toggleClass('open');
+                $menu.css('display', $menu.hasClass('open') ? 'block' : 'none');
+            });
+    
     $('.wpp-popup-content').each(function(){
         $(this).wrap('<div class="wpp-popup-wrapper"><div class="wpp-popup-inside">');
       });
